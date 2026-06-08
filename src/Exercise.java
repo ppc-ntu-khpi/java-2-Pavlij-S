cb58e8c4af7836517f10a2ef749f286f4faa5fda
Pavlij Sofia <pavlijsofia88@gmail.com>
2026-06-08 16:33:40 +0000

    “Task 13 execution (Caesar cipher), updating Exercise.java and TestResult.java

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,32 @@
 //package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    // Зашифровать фразу шифром Цезаря
+    public static String Calculate(String phrase, int shift) {
+        if (phrase == null) {
+            return null;
+        }
+
+        int normalizedShift = shift % 26;
+        if (normalizedShift < 0) {
+            normalizedShift += 26;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < phrase.length(); i++) {
+            char c = phrase.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                char encrypted = (char) ('A' + (c - 'A' + normalizedShift) % 26);
+                result.append(encrypted);
+            } else if (c >= 'a' && c <= 'z') {
+                char encrypted = (char) ('a' + (c - 'a' + normalizedShift) % 26);
+                result.append(encrypted);
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
     }
 }
